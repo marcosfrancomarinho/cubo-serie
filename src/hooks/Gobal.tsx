3d21f19358599a764e7c1f3d2d9c225a7c502d2a
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-12-07 21:56:55 -0300

    'inicio'

--- src/hooks/Gobal.tsx
+++ src/hooks/Gobal.tsx
@@ -1,78 +1,34 @@
 import React, { ReactNode } from 'react';
 import { BrowserRouter, Routes } from 'react-router-dom';
-import { Context } from './Context';
+import { Context, ValuesParams } from './Context';
+
 interface ParamsGlobal {
     children: ReactNode;
 }
 
-
 const Global: React.FC<ParamsGlobal> = ({ children }) => {
-    return (
-        <Context.Provider
-            value={{
-                images: [
-                    {
-                        title: 'aneis',
-                        url: '/images/aneis.jpg',
-                    },
-                    {
-                        title: 'bad',
-                        url: '/images/bad.jpg',
-                    },
-                    {
-                        title: 'dark',
-                        url: '/images/dark.jpg',
-                    },
-                    {
-                        title: 'game',
-                        url: '/images/game.jpg',
-                    },
-                    {
-                        title: 'pinguim',
-                        url: '/images/Pinguim.jpg',
-                    },
-                    {
-                        title: 'quarta',
-                        url: '/images/quarta.jpg',
-                    },
-                    {
-                        title: 'saul',
-                        url: '/images/saul.jpg',
-                    },
-                    {
-                        title: 'sopranos',
-                        url: '/images/sopranos.jpg',
-                    },
-                    {
-                        title: 'string',
-                        url: '/images/string.jpg',
-                    },
-                    {
-                        title: 'the last',
-                        url: '/images/the_last.jpg',
-                    },
-                    {
-                        title: 'witcher',
-                        url: '/images/witcher.jpg',
-                    },
-                    {
-                        title: 'lost',
-                        url: '/images/lost.jpg',
-                    },
-                    {
-                        title: 'office',
-                        url: '/images/office.jpg',
-                    },
-                ],
-                menu: ['Inicio', 'Registro', 'Suporte'],
-                title: 'Cubo Series',
-            }}
-        >
-            <BrowserRouter>
-                <Routes>{children}</Routes>
-            </BrowserRouter>
-        </Context.Provider>
-    );
+    const [datas, setDatas] = React.useState<ValuesParams | null>(null);
+    React.useEffect(() => {
+        fetch('/src/json/db.json')
+            .then((res) => res.json())
+            .then((json) => setDatas(json));
+    }, [datas]);
+    if (datas) {
+        return (
+            <Context.Provider
+                value={{
+                    images: datas.images,
+                    menu: datas.menu,
+                    title: datas.title,
+                }}
+            >
+                <BrowserRouter>
+                    <Routes>{children}</Routes>
+                </BrowserRouter>
+            </Context.Provider>
+        );
+    }
+    return null;
 };
 
 export default Global;
