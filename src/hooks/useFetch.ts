fb3bbe02df3b265369edc9c6faee643dab08353c
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-12-29 11:52:07 -0300

    'finalizado'

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,14 +1,14 @@
-import React from "react";
-import { ValuesParams } from "../hooks/Context";
-
+import React from 'react';
+import { ValuesParams } from '../hooks/Context';
 
 export function useFetch(url: string): ValuesParams | null {
-    const [datas, setDatas] = React.useState<ValuesParams | null>(null);
-    React.useEffect(() => {
-        fetch(url)
-            .then((res) => res.json())
-            .then((json) => setDatas(json));
-    }, [url]);
-    return datas;
+	const [datas, setDatas] = React.useState<ValuesParams | null>(null);
+	React.useMemo(() => {
+		fetch(url)
+			.then((res) => res.json())
+			.then((json) => setDatas(json))
+			.catch(() => setDatas(null));
+            console.log('entrou')
+	}, [url]);
+	return datas;
 }
-
